1af7495e7319b66910ff552b10e927b140bc10b9
ashishv120 <66671856+ashishv120@users.noreply.github.com>
2023-05-21 19:06:35 +0530

    Update checkRam.sh

--- checkRam.sh
+++ checkRam.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-##################
+####################
 #Author: Ashish
 #Version: v1
 #
 # This script is used to check the utilization of the Memory Managament for RAM usage and helps user to check the details on the server. 
 #
-##################
+####################
 
-free | awk '{print $1 " " $2 " " $3}' | awk '(NR>1)' | while read output;
+free -h | awk '{print $1 " " $2 " " $3}' | awk '(NR>1)' | while read output;
 do
 echo "RAM Details: $output"
 done
 
-#######
+####################
 # Comments used and details of it
 # free command is a great way to monitor RAM usage on a server
 # '-h' gives the details in human readable format (converts digits to 3 digit number)
 # (NR>1) skips the first row
 # 
+####################
 
