67d020eb39f04e474f2abada68c209615abf89c1
ashishv120 <66671856+ashishv120@users.noreply.github.com>
2023-05-22 13:52:34 +0530

    Update checkDISK_usage.sh

--- checkDISK_usage.sh
+++ checkDISK_usage.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-alert=15
+#####################
+# Author: Ashish
+# Version: v1
+#
+# This script is used to check the utilization of the DISK Managament for DISK usage and helps user to check these details on the server. 
+#
+#####################
+
+alert=15 #used memory in percentage
 echo "!!!!!Details of DISK Usage!!!!!"
 df -H | awk '{print $5 "  " $1 " " $6}' | while read output;
 do
@@ -12,6 +20,14 @@ do
   echo "Critical for $file_sys mounted on $mount_Loc" 
 fi
 done
+
+#####################
+# df command will display information about all mounted file systems
+# '-H' gives the details in human readable format (kilobytes, megabytes, gigabytes and so on)
+# cut command is used to cut a specific section of a file, -d is used to cut a specific section by a delimiter
+# '-f' is used to select the specific fields (eg: -f1 gives the result of col1 & f2 gives result of col2
+#
+#####################
 
 
 
